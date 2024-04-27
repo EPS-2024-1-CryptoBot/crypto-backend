3c4d4cbfc3e68828ba2dc435d2f8da514fce2b31
Kevin Luis <k.luis360@gmail.com>
2024-04-27 17:13:54 -0300

    feat(app): add health checker

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,12 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @UseGuards(AuthGuard('jwt'))
-  @Get('/hello')
-  getHello(@Req() request: Request): string {
-    console.log('User', JSON.stringify(request['user']));
-    return 'Hello ' + JSON.stringify(request['user']) + '!';
+  @Get('/health')
+  healthCheck() {
+    return true;
   }
 }
