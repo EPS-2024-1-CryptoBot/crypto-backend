285c9eb8bf6a124a1dc22e1b672ac5f94ed98600
Ailton Aires <ailtonaires5@gmail.com>
2024-05-23 17:15:25 -0300

    Adicionando chamadas de criptografia da key da binance no consultant

--- src/consultant/consultant.module.ts
+++ src/consultant/consultant.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConsultantController } from './consultant.controller';
 import { ConsultantService } from './consultant.service';
 import { ConsultantApiService } from '@/consultant';
+import { RsaApiService } from '@/rsa-api';
+import {UserService} from '../user/user.service';
 
 @Module({
   controllers: [ConsultantController],
-  providers: [ConsultantService, ConsultantApiService],
+  providers: [ConsultantService, ConsultantApiService, RsaApiService, UserService],
 })
 export class ConsultantModule {}
--- src/consultant/consultant.service.ts
+++ src/consultant/consultant.service.ts
@@ -1,9 +1,13 @@
 import { ConsultantApiService } from '@/consultant';
+import { RsaApiService } from '@/rsa-api';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ConsultantService {
-  constructor(private readonly consultantApiService: ConsultantApiService) {}
+  constructor(
+    private readonly consultantApiService: ConsultantApiService,
+    private readonly RsaApiService: RsaApiService,
+  ) {}
 
   async getCoinList() {
     return await this.consultantApiService.coinList();
@@ -15,5 +19,37 @@ export class ConsultantService {
 
   async getCoinHistory(coin: string) {
     return await this.consultantApiService.coinHistory(coin);
+  }
+
+  async addApiKeyBinanceToUser(firebaseUid: string) {
+    try {
+      // const response = await this.consultantApiService.getApiKeyBinanceToUser(
+      //   firebaseUid,
+      // );
+      const api_key_binance = 'asdASANLjslnaDLnas8su89oh312yedquasd';
+      const public_key = process.env.SYSTEM_PUB_K;
+      const response_rsa = await this.RsaApiService.encrypt(
+        api_key_binance,
+        public_key,
+      );
+      return response_rsa.data.encrypted_message;
+    } catch (error) {
+      console.error('ALERTA DE ERRO: ', error);
+      throw new Error(error);
+    }
+  }
+
+  async getApiKeyBinanceToUser(api_key_binance: string) {
+    try {
+      const private_key = process.env.SYSTEM_PVT_K;
+      const response = await this.RsaApiService.decrypt(
+        api_key_binance,
+        private_key,
+      );
+      return response.data.decrypted_message;
+    } catch (error) {
+      console.error('ALERTA DE ERRO: ', error);
+      throw new Error(error);
+    }
   }
 }
